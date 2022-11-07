fc0786d6f4c53e42b20d93e7c3213ce89433bb57
Franz Amador <franzamador@gmail.com>
2022-11-07 13:31:16 -0800

    [Refactor] Extract fizzbuzz method for testing and elaboration

--- src/main/java/fga/fizzbuzz/FizzBuzz.java
+++ src/main/java/fga/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,11 @@ package fga.fizzbuzz;
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int num = 1; num <= 100; num++) {
-            System.out.println(String.valueOf(num));
+            System.out.println(fizzbuzz(num));
         }
+    }
+
+    private static String fizzbuzz(int num) {
+        return String.valueOf(num);
     }
 }
