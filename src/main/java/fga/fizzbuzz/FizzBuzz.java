c17c7c063e2be349d03c93dbcd84cf2af1f7c9a8
Franz Amador <franzamador@gmail.com>
2022-11-07 13:30:23 -0800

    [Refactor] Make int-to-String conversion explicit

--- src/main/java/fga/fizzbuzz/FizzBuzz.java
+++ src/main/java/fga/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,7 @@ package fga.fizzbuzz;
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int num = 1; num <= 100; num++) {
-            System.out.println(num);
+            System.out.println(String.valueOf(num));
         }
     }
 }
