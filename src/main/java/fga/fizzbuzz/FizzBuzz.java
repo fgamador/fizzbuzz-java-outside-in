884fa9d6a84f0ee9c25d066e65f7d15cb237a17c
Franz Amador <franzamador@gmail.com>
2022-11-07 13:28:54 -0800

    Walking skeleton

---
+++ src/main/java/fga/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,9 @@
+package fga.fizzbuzz;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int num = 1; num <= 100; num++) {
+            System.out.println(num);
+        }
+    }
+}
