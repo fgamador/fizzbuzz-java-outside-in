6dc4bf255e2afba33fff41d1dd06871b358ba94a
Franz Amador <franzamador@gmail.com>
2022-11-17 17:07:34 -0800

    [Refactor] Generalize fizzbuzz function into real implementation

--- src/main/java/fga/fizzbuzz/FizzBuzz.java
+++ src/main/java/fga/fizzbuzz/FizzBuzz.java
@@ -8,7 +8,7 @@ public class FizzBuzz {
     }
 
     static String fizzbuzz(int num) {
-        if (num == 30)
+        if (num % 15 == 0)
             return "FizzBuzz";
 
         if (num % 5 == 0)
